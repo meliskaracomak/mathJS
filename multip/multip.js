2c85f601bbd0a2a3ebbc3032da997b52cbd75382
Melis Karaçomak <56133405+meliskaracomak@users.noreply.github.com>
2020-10-20 11:39:47 +0300

    Add files via upload

---
+++ multip/multip.js
@@ -0,0 +1,22 @@
+function multiplyBy(){
+    num1 = document.getElementById("firstNumber").value;
+    num2 = document.getElementById("secondNumber").value;
+    document.getElementById("result").innerHTML = num1 * num2;
+}
+function divideBy(){
+    num1 = document.getElementById("firstNumber").value;
+    num2 = document.getElementById("secondNumber").value;
+    document.getElementById("result").innerHTML = num1 / num2;
+}
+
+function additionBy(){
+    num1 = document.getElementById("firstNumber").value;
+    num2 = document.getElementById("secondNumber").value;
+    document.getElementById("result").innerHTML = num1 + num2;
+}
+
+function subtructionBy(){
+    num1 = document.getElementById("firstNumber").value;
+    num2 = document.getElementById("secondNumber").value;
+    document.getElementById("result").innerHTML = num1 - num2;
+}
